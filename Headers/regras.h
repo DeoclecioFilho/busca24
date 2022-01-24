c635714426bc8103d9bc0e887e4a5822840e189b
DeoclécioFilho <deoclecio.filho@estudante.ufjf.br>
2022-01-24 15:04:56 -0300

    atualizado

--- Headers/regras.h
+++ Headers/regras.h
@@ -4,7 +4,9 @@
 using namespace std;
 
 const int NUM = 5;
-
+const int RAIZ = 65;
+const int RESULT = 24;
+/*
 void r1(int *jogo, int &v1, int &v2, int &v3, int &v4);
 void r2(int *jogo, int &v1, int &v2, int &v3, int &v4);
 void r3(int *jogo, int &v1, int &v2, int &v3, int &v4);
@@ -20,5 +22,19 @@ void r10(int *jogo, int &v1, int &v2, int &v3, int &v4);
 void r13(int *jogo, int &v1, int &v2, int &v3, int &v4);
 void r14(int *jogo, int &v1, int &v2, int &v3, int &v4);
 void r15(int *jogo, int &v1, int &v2, int &v3, int &v4);
+*/
+void estado(int *jogo);
+void idNo(char *no, int i);
+int valorCartas(int *jogo, int i);
+void filhoRaiz(int *jogo, int &filho);
+vector<int> gravaFila(int *jogo, vector<char> &noFilho, int &filho);
+void filaRegras(vector<char> noFilho, vector<int> fila);
+void listaAbertos(vector<string> &lstAbertos);
+void listaFechados(vector<string> &lstFechados);
+void concat(char letra, char texto[]);
+void concStrCharF(string k, char f, vector<string> &lstFechados);
+void concStrCharA(string k, char f, vector<string> &lstAbertos);
+void menorFilho(vector<char> noFilho, vector<int> fila,
+                vector<string> &lstAbertos);
 
 #endif /* C56D52EA_FECC_43F3_A9F1_2E6579F5B34E */
--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,4 +1,48 @@
-#include "regras.h"
+#include "Headers\regras.h"
 #include "stdc++.h"
-
+#include "Headers" 
 using namespace std;
+
+
+int main() {
+
+  int jogo[NUM] = {0, 4, 6, 5, 3}, final[NUM] = {24, 0, 0, 0, 0}, filho = RAIZ;
+
+  vector<char> noFilho;
+  vector<int> fila;
+  vector<string> lstAbertos, lstFechados;
+  string k;
+  char f;
+
+  cout << "\n\n";
+  cout << "   *** BUSCA ORDENADA ***  \n\n";
+  cout << " - Critério de desempate - \n";
+  cout
+      << " Carta de menor valor ou ordem de entrada na lista de abertos.  \n\n";
+  cout << "Estado inicial: ";
+  cout << char(RAIZ) << " = ";
+  estado(jogo);
+  cout << "\n";
+  cout << "Estado final: ";
+  estado(final);
+  cout << endl;
+  cout << "Filhos da Raiz: ";
+  filhoRaiz(jogo, filho);
+
+  fila = gravaFila(jogo, noFilho, filho);
+  filaRegras(noFilho, fila);
+
+  // inicio (0)
+  k = "(0)";
+  f = char(RAIZ);
+  concStrCharF(k, f, lstFechados);
+  listaFechados(lstFechados);
+  menorFilho(noFilho, fila, lstAbertos);
+  listaAbertos(lstAbertos);
+
+  //
+  cout << "filho: " << filho;
+  cout << endl << endl;
+
+  return 0;
+}
